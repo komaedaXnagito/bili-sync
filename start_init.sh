e3a672040e14394332474433d7e84a582f7cd513
komaedaXnagito <3107446179@qq.com>
2024-01-20 16:50:11 +0800

    feat: 测试

--- start_init.sh
+++ start_init.sh
@@ -1,9 +1,10 @@
-branch=$BILI_SYNC_VERSION
-git clean -dffx
-git fetch --depth 1 origin $BILI_SYNC_VERSION
-git reset --hard origin/$BILI_SYNC_VERSION
-git submodule update --init --recursive
-# 安装依赖
-poetry install --only main --no-root
+#branch=$BILI_SYNC_VERSION
+#git clean -dffx
+#git fetch --depth 1 origin $BILI_SYNC_VERSION
+#git reset --hard origin/$BILI_SYNC_VERSION
+#git submodule update --init --recursive
+## 安装依赖
+#poetry install --only main --no-root
 # 修复权限
 #chown -R nt:nt /$WORKDIR
+echo "TEST"
